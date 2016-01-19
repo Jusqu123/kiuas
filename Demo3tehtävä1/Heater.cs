9584e43fd30b228b482c2b091d58ac099c5c668f
Jusqu123 <jusquu123@gmail.com>
2016-01-19 10:40:30 +0200

    gg

---
+++ Demo3tehtävä1/Heater.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3tehtävä1
+{
+    class Heater
+    {
+        //properties
+        public bool IsOn { get; set; }
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+
+
+        //method prints heater data
+        public void PrintData()
+        {
+
+            Console.WriteLine("Heater : ");
+            Console.WriteLine("- Temperature :{0} Humidity {1}: IsOn: ",Temperature,Humidity,IsOn);
+         
+        }
+    }
+}
---
+++ Demo3tehtävä1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3tehtävä1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a new heater object
+            Heater heater = new Heater();
+            heater.IsOn = true;
+            heater.Temperature = 100;
+            heater.Humidity = 95;
+            heater.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
